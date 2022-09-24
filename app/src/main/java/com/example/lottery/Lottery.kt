544668ff06f1bc0c327691fee8c1288acad0dafc
jeongsanghyeon <dev.jusa@gmail.com>
2022-09-24 12:21:32 +0900

    Define Interface

---
+++ app/src/main/java/com/example/lottery/Lottery.kt
@@ -0,0 +1,13 @@
+package com.example.lottery
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Lottery(
+    modifier: Modifier,
+    showContentCallback: ()->Unit,
+    content: @Composable ()->Unit
+){
+    // TODO
+}
--- app/src/main/java/com/example/lottery/MainActivity.kt
+++ app/src/main/java/com/example/lottery/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.lottery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
+            Lottery(
+                modifier = Modifier.size(300.dp, 100.dp),
+                showContentCallback = ::showContentCallback
+            ){
+                TODO()
+            }
         }
+    }
+
+    private fun showContentCallback() {
+        TODO("Not yet implemented")
     }
 }
